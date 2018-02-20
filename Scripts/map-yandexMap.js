0411b5b41475a37cfd9c4329e42af944e40335f3
Ekaterina L <tarrenfuere@gmail.com>
2018-02-20 19:05:20 +0300

    load optimization + map zoom

--- Scripts/map-yandexMap.js
+++ Scripts/map-yandexMap.js
@@ -9,28 +9,48 @@ $(document).ready(function() {
             zoom: 11
         });
 
-        var burger1 = new ymaps.Placemark([59.970609, 30.310975], {}, {
+        var burger1 = new ymaps.Placemark([59.970609, 30.310975], {
+            hintContent: 'Ресторан на Попова',
+            balloonContentHeader: 'Ресторан на Попова',
+            balloonContentBody: 'Мы всегда рады угостить Вас вкусными бургерами!',
+            balloonContentFooter: 'Заходите в гости!',
+        }, {
             iconLayout: 'default#image',
             iconImageHref: 'IMG8/map-marker.png',
             iconImageSize: [46, 57],
-            iconImageOffset: [-23, -57]
+            iconImageOffset: [-23, -57],
         }); // burger 1
 
-        var burger2 = new ymaps.Placemark([59.945041, 30.382558], {}, {
+        var burger2 = new ymaps.Placemark([59.945041, 30.382558], {
+            hintContent: 'Ресторан на Кавалерградской',
+            balloonContentHeader: 'Ресторан на Кавалерградской',
+            balloonContentBody: 'Мы всегда рады угостить Вас вкусными бургерами!',
+            balloonContentFooter: 'Заходите в гости!',
+        }, {
             iconLayout: 'default#image',
             iconImageHref: 'IMG8/map-marker.png',
             iconImageSize: [46, 57],
             iconImageOffset: [-23, -57]
         }); // burger 2
 
-        var burger3 = new ymaps.Placemark([59.915834, 30.492936], {}, {
+        var burger3 = new ymaps.Placemark([59.915834, 30.492936], {
+            hintContent: 'Ресторан на Подвойского',
+            balloonContentHeader: 'Ресторан на Подвойского',
+            balloonContentBody: 'Мы всегда рады угостить Вас вкусными бургерами!',
+            balloonContentFooter: 'Заходите в гости!',
+        }, {
             iconLayout: 'default#image',
             iconImageHref: 'IMG8/map-marker.png',
             iconImageSize: [46, 57],
             iconImageOffset: [-23, -57]
         }); // burger 3
 
-        var burger4 = new ymaps.Placemark([59.888039, 30.315425], {}, {
+        var burger4 = new ymaps.Placemark([59.888039, 30.315425], {
+            hintContent: 'Ресторан на Заставкой',
+            balloonContentHeader: 'Ресторан на Заставкой',
+            balloonContentBody: 'Мы всегда рады угостить Вас вкусными бургерами!',
+            balloonContentFooter: 'Заходите в гости!'
+        }, {
             iconLayout: 'default#image',
             iconImageHref: 'IMG8/map-marker.png',
             iconImageSize: [46, 57],
@@ -38,11 +58,12 @@ $(document).ready(function() {
         }); // burger 4
 
         myMap.geoObjects.add(burger1);
+        // burger1.balloon.open();
         myMap.geoObjects.add(burger2);
         myMap.geoObjects.add(burger3);
         myMap.geoObjects.add(burger4);
 
-        // myMap.behaviors.disable(‘scrollZoom’);
+        myMap.behaviors.disable('scrollZoom'); 
     }
 
 
